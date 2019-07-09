21076b8fe1d3d572a7f4eb764413f4b992bb952b
Vishwas Paikra <vishwaspaikra007@gmail.com>
2019-07-10 03:12:27 +0530

    parallex #1 experiments

---
+++ parallex 1 imagination/main.js
@@ -0,0 +1,14 @@
+var mainContainer = document.querySelector(".mainContainer")
+var box1 = document.querySelector("#box1")
+var box2 = document.querySelector("#box2")
+
+mainContainer.onscroll = function() { parallex()};
+ function parallex() {
+    var scroll1 = mainContainer.scrollTop;
+    var height1 = mainContainer.scrollHeight - document.documentElement.clientHeight;
+    var scrolled1 = (scroll1/height1)*100;
+    box1.style.marginLeft = "-" + (box1.clientWidth/100)*scrolled1 + "px";
+    box2.style.marginLeft = box1.clientWidth + 2*(box2.clientWidth/100)*scrolled1 + "px";
+    box1.style.transform = `rotate(${scrolled1}deg)`;
+
+ }
