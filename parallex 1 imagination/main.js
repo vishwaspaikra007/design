dc08880162b1934cd411661eebb31ab700467f1f
Vishwas Paikra <vishwaspaikra007@gmail.com>
2019-07-13 00:54:08 +0530

    added blur effect for banner and my-image

--- parallex 1 imagination/main.js
+++ parallex 1 imagination/main.js
@@ -5,6 +5,17 @@ var body = document.querySelector("body")
 var meLooking = document.querySelector(".meLooking")
 var dialogue1 = document.querySelector(".dialogue1")
 var dialogueAnimator = document.querySelectorAll(".dialogueAnimator")
+var dialogue1 = document.querySelector(".dialogue1")
+var containerForBox = document.querySelector(".containerForBox")
+
+dialogue1.addEventListener("mouseover",(e)=> {
+    dialogue1.style.filter = "blur(0.5px)";
+    meLooking.style.filter = "blur(5px)";
+});
+dialogue1.addEventListener("mouseout",(e)=> {
+    dialogue1.style.filter = "blur(5px)";
+    meLooking.style.filter = "blur(0px)";
+});
 
 mainContainer.onscroll = function() { parallex()};
  function parallex() {
@@ -46,6 +57,12 @@ mainContainer.onscroll = function() { parallex()};
         dialogueAnimator[0].style.height = "100%";
         dialogueAnimator[1].style.transition = "1s ease-in";
         dialogueAnimator[1].style.height = "100%";
+    }
+    if(scrolled1 > 80)
+    {
+        containerForBox.style.zIndex = "0";
+    } else {
+        containerForBox.style.zIndex = "3";
     }
     box1.style.transition = '0s';
     box1.style.marginLeft = "-" + (box1.clientWidth/100)*scrolled1 + "px";
