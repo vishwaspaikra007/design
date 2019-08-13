1fb810a63a0d11fe2d2acea87fbaf6c440ed6270
Vishwas Paikra <vishwaspaikra007@gmail.com>
2019-08-13 14:43:23 +0530

    components to design a web based text editor

---
+++ selection/selection.js
@@ -0,0 +1,13 @@
+
+function textbox()
+{
+    //document.execCommand(aCommandName, aShowDefaultUI, aValueArgument)
+    document.execCommand('backColor', true, 'orange');
+}
+function textbox2() {
+    document.execCommand('bold', false);
+}
+function textbox3()
+{
+    document.execCommand('justifyCenter', false);
+}
